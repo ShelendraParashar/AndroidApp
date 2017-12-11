2a911b72bf64a13b7b6a75c22aff17b1baeb9f12
Anand Shukla <shelendra.1989@gmail.com>
2017-12-11 15:05:43 +0530

    rejecting test class deta

--- src/main/java/com/weather/testClass.java
+++ src/main/java/com/weather/testClass.java
@@ -13,56 +13,11 @@ import org.openqa.selenium.Capabilities;
 import org.openqa.selenium.remote.DesiredCapabilities;
 
 public class testClass {
-	char d;
-	
-	static FileReader reader;
-	static Properties prop;
-	static DesiredCapabilities caps = new DesiredCapabilities();
-	
-	public static void loadConfig() {
 
-		try {
-			reader = new FileReader("D:\\Twc-Android-Automation\\AndroidApp-master\\config.properties");
-		} catch (FileNotFoundException e) {
-			e.printStackTrace();
-		}
-		prop = new Properties();
-		try {
-			prop.load(reader);
-			// prop.getProperty("capability.app");
-
-		} catch (IOException e) {
-			// TODO Aut o-generated catch block
-			e.printStackTrace();
-		}
-
-	}
 	
-	
-	public static void reaCap() {
+	public static void main(String[] args)  {
 		
-		Set set= prop.entrySet();
 		
-		Iterator itr=set.iterator();  
-		
-		while(itr.hasNext()){  
-			Map.Entry entry=(Map.Entry)itr.next();  
-			
-			String fullKey=(String) entry.getKey();
-			
-			String capabilityStart="capability";
-			if(fullKey.contains(capabilityStart)) {
-				
-				String capabilityKey=fullKey.substring(capabilityStart.length()+1, fullKey.length());
-				fullKey= fullKey.replaceAll("capability.", "");
-				
-				System.out.println(fullKey+"   "+entry.getValue());
-			}
-		}
-		
-	}
-	
-	public static void main(String[] args) throws UnsupportedEncodingException {
 	
 		
 	}
